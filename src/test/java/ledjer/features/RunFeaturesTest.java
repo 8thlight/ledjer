26153331006e9fdc2cdc4d721a0f145e2dfabf65
Mateu Adsuara <mateuadsuara@gmail.com>
2015-09-21 15:30:45 +0100

    Adds exercises as Cucumber features

---
+++ src/test/java/ledjer/features/RunFeaturesTest.java
@@ -0,0 +1,13 @@
+package ledjer.features;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.SnippetType;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(snippets = SnippetType.CAMELCASE)
+public class RunFeaturesTest
+{
+}
